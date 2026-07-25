5b7b51fc4757adf416530bf2dfafa4f894430fd4
lydaramadhani <lydaramadhani05@gmail.com>
2026-07-25 14:57:02 +0700

    Membuat modul pemesanan tiket peserta

--- events/views.py
+++ events/views.py
@@ -1,3 +1,4 @@
+# Modul pemesanan tiket khusus peserta event
 import datetime
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout, authenticate
